745cdc916de709904220ab4dfcc43125013b3ff8
Gashap <46971365+Gashap@users.noreply.github.com>
2021-12-19 13:10:19 +0500

    it's all

--- src/GL6_PassOb.java
+++ src/GL6_PassOb.java
@@ -16,7 +16,7 @@ class  Block{
     }
 
     //Вернуть логическое значение true, если параметр ob определяет переллелепипед того же объёма
-    boolean sameVolume(Block ob){
+    boolean sameVolume(Block ob){   //объект в качестве параметра
         return ob.volume == volume;
     }
 }
@@ -27,8 +27,8 @@ public class GL6_PassOb {
         Block ob2 = new Block(10, 2, 5);
         Block ob3 = new Block(4, 5, 5);
 
-        System.out.println(" ob имеет те же размеры, что и ob2: " + ob1.sameBlock(ob2));
-        System.out.println(" ob имеет те же размеры, что и ob3: " + ob1.sameBlock(ob3));
-        System.out.println(" ob имеет тот же объём, что и ob3: " + ob1.sameVolume(ob3));
+        System.out.println("ob1 имеет те же размеры, что и ob2: " + ob1.sameBlock(ob2));
+        System.out.println("ob1 имеет те же размеры, что и ob3: " + ob1.sameBlock(ob3));
+        System.out.println("ob1 имеет тот же объём, что и ob3: " + ob1.sameVolume(ob3));
     }
 }
