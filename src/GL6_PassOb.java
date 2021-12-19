b9455ac98d365d9cec6f2ba7f88c34710ed3300e
Gashap <46971365+Gashap@users.noreply.github.com>
2021-12-19 13:13:45 +0500

    it's all

--- src/GL6_PassOb.java
+++ src/GL6_PassOb.java
@@ -28,7 +28,7 @@ public class GL6_PassOb {
         Block ob3 = new Block(4, 5, 5);
 
         System.out.println("ob1 имеет те же размеры, что и ob2: " + ob1.sameBlock(ob2));
-        System.out.println("ob1 имеет те же размеры, что и ob3: " + ob1.sameBlock(ob3));
+        System.out.println("ob2 имеет те же размеры, что и ob3: " + ob2.sameBlock(ob3));
         System.out.println("ob1 имеет тот же объём, что и ob3: " + ob1.sameVolume(ob3));
     }
 }
