770a244e75b586fb1989d59e3ea0a86b0e00cf36
Gashap <46971365+Gashap@users.noreply.github.com>
2021-12-19 13:05:48 +0500

    it's all

---
+++ src/GL6_PassOb.java
@@ -0,0 +1,34 @@
+//Методам можно передовать объекты
+class  Block{
+    int a, b, c;
+    int volume;
+
+    Block(int i, int j, int k){
+        a = i;
+        b = j;
+        c = k;
+        volume = a*b*c;
+    }
+
+    //Вернуть логическое значение true, если параметр ob определяет тот же переллелепипед
+    boolean sameBlock(Block ob){    //объект в качестве параметра
+        return (ob.a == a) & (ob.b == b) & (ob.c == c);
+    }
+
+    //Вернуть логическое значение true, если параметр ob определяет переллелепипед того же объёма
+    boolean sameVolume(Block ob){
+        return ob.volume == volume;
+    }
+}
+
+public class GL6_PassOb {
+    public static void main(String[] args) {
+        Block ob1 = new Block(10, 2, 5);
+        Block ob2 = new Block(10, 2, 5);
+        Block ob3 = new Block(4, 5, 5);
+
+        System.out.println(" ob имеет те же размеры, что и ob2: " + ob1.sameBlock(ob2));
+        System.out.println(" ob имеет те же размеры, что и ob3: " + ob1.sameBlock(ob3));
+        System.out.println(" ob имеет тот же объём, что и ob3: " + ob1.sameVolume(ob3));
+    }
+}
